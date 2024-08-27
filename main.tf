0fdb63c1e644f38b8a910d5f9e7e256485e9ca36
shaheel03 <shaheelkumar@gmail.com>
2024-08-28 00:28:55 +0530

    rg added

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
-resource "azurerm_resource_group" "name" {
-    name = "nhbgfd"
+resource "azurerm_resource_group" "rg1" {
+    name = "rg1"
 location = "east us"
 }  
-resource "azurerm_resource_group" "nh" {
-    name = "nhbgfdb"
+resource "azurerm_resource_group" "rg2" {
+    name = "rg2"
 location = "west us"
 }  
+resource "azurerm_resource_group" "rg3" {
+    name = "rg3"
+location = "central india"
+}  
+resource "azurerm_resource_group" "rg4" {
+    name = "rg4"
+location = "west us"
+}  
+
 
 
  
